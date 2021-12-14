f7e1a5b07f6df64186213b4a2551f0d790ce205e
AsliddinWeb <asliddinsinger@gmail.com>
2021-12-14 08:31:38 +0500

    22

--- js/app.js
+++ js/app.js
@@ -9,27 +9,18 @@ const burger = document.querySelector("#showHide"),
       ssInput = document.querySelector(".ss-menu"),
       blur = document.querySelectorAll(".blur");
 
-function myFunction(x) {
-    if (x.matches) { // If media query matches
-        burger.addEventListener("click", () => {
-            burgercha.classList.toggle("navnav");
-            for (let i = 0; i < blur.length; ++i) {
-                blur[i].classList.toggle("blurr");
-            }
-            console.log('test1');
-        });
-        hide.addEventListener("click", () => {
-            burgercha.classList.toggle("navnav");
-            for (let i = 0; i < blur.length; ++i) {
-                blur[i].classList.toggle("blurr");
-            }
-            console.log('test2');
-        });
-    } else {
-        console.log("else");
-    }
-  }
-  
-  var x = window.matchMedia("(max-width: 1399.98px)")
-  myFunction(x) // Call listener function at run time
-  x.addListener(myFunction) // Attach listener function on state changes
+      burger.addEventListener("click", () => {
+        burgercha.classList.toggle("navnav");
+        for (let i = 0; i < blur.length; ++i) {
+            blur[i].classList.toggle("blurr");
+        }
+        console.log('test1');
+    });
+
+    hide.addEventListener("click", () => {
+        burgercha.classList.toggle("navnav");
+        for (let i = 0; i < blur.length; ++i) {
+            blur[i].classList.toggle("blurr");
+        }
+        console.log('test2');
+    });
